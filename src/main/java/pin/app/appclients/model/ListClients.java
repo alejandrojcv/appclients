54d7660edc29d1f83a838b15a21271722c93c49e
Alejandro Canadas <alejandro.canadas@claro.com.ar>
2023-03-13 15:43:07 -0300

    Delete lombok

--- src/main/java/pin/app/appclients/model/ListClients.java
+++ src/main/java/pin/app/appclients/model/ListClients.java
@@ -3,16 +3,12 @@ package pin.app.appclients.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
+
 public class ListClients {
     private int id;
     private String nombre;
